558dab676a86125291a82aaf01bae676f9fcde87
Ajay <helloajaysingh1@gmail.com>
2020-04-22 20:35:56 +0530

    Bug : the picture is'nt replacing with the image view

--- app/src/main/java/com/example/todonotesapp/view/AddNotesActivity.kt
+++ app/src/main/java/com/example/todonotesapp/view/AddNotesActivity.kt
@@ -2,6 +2,7 @@ package com.example.todonotesapp.view
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,8 +14,11 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.todonotesapp.R
+import java.util.jar.Manifest
 
 class AddNotesActivity : AppCompatActivity() {
 
@@ -25,6 +29,7 @@ class AddNotesActivity : AppCompatActivity() {
     val REQUEST_CODE_GALLERY = 2
     val REQUEST_CODE_CAMERA = 1
     var picturePath = ""
+    var MY_PERMISSION_CODE = 124
     val TAG = "AddNotesActivity"
 
 
@@ -39,10 +44,42 @@ class AddNotesActivity : AppCompatActivity() {
     private fun clickListeners() {
         imageViewNotes.setOnClickListener(object: View.OnClickListener{
             override fun onClick(v: View?) {
-                setupDialog()
+                if(checkAndRequestPermission()) {
+                    setupDialog()
+                }
             }
 
         })
+    }
+
+    private fun checkAndRequestPermission(): Boolean {
+        val cameraPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        val storagePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        val listPermissionNeeded = ArrayList<String>()
+
+        if(cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(android.Manifest.permission.CAMERA)
+        }
+        if(storagePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if(listPermissionNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this,listPermissionNeeded.toTypedArray<String>(), MY_PERMISSION_CODE)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            MY_PERMISSION_CODE ->{
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // permission is granted
+                    setupDialog()
+                }
+            }
+        }
     }
 
     private fun setupDialog() {
@@ -65,6 +102,7 @@ class AddNotesActivity : AppCompatActivity() {
             override fun onClick(v: View?) {
                 val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(intent, REQUEST_CODE_GALLERY)
+                    dialog.hide()
             }
 
         })
